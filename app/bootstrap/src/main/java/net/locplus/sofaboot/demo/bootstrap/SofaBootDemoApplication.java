445d8dd6f2343d37e66d126af99f4e3b15dbdeb5
shuangxi.dsx <dingshuangxi888@gmail.com>
2022-05-01 21:41:32 +0800

    初始化项目

---
+++ app/bootstrap/src/main/java/net/locplus/sofaboot/demo/bootstrap/SofaBootDemoApplication.java
@@ -0,0 +1,11 @@
+package net.locplus.sofaboot.demo.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaBootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SofaBootDemoApplication.class, args);
+    }
+}
