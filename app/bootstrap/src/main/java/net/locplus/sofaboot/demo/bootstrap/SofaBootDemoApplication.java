bcc7e257800a1e41d1060990033f24536d57ec01
shuangxi.dsx <dingshuangxi888@gmail.com>
2022-05-04 21:28:48 +0800

    支持spring上下文隔离

--- app/bootstrap/src/main/java/net/locplus/sofaboot/demo/bootstrap/SofaBootDemoApplication.java
+++ app/bootstrap/src/main/java/net/locplus/sofaboot/demo/bootstrap/SofaBootDemoApplication.java
@@ -3,7 +3,7 @@ package net.locplus.sofaboot.demo.bootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "net.locplus.sofaboot.demo.web")
 public class SofaBootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SofaBootDemoApplication.class, args);
